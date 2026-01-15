95cce41600fc63283a22aadd08ef783c32633ae2
Pawana Shettigar <teampawana@gmail.com>
2026-01-15 16:13:35 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -537,37 +537,21 @@ $(document).ready(function() {
 });
 $(document).ready(function() {
     
-    // 1. Typing Effect Logic
-    function typeTitle($element) {
-        var text = $element.attr('data-title') || $element.text();
-        if (!$element.attr('data-title')) $element.attr('data-title', text);
-        
-        $element.text('');
-        var i = 0;
-        var timer = setInterval(function() {
-            if (i < text.length) {
-                $element.append(text.charAt(i));
-                i++;
-            } else {
-                clearInterval(timer);
-            }
-        }, 60);
-    }
-
-    // 2. Click Star Card (Go to Poem)
+   /* --- NAVIGATION WITHOUT TYPING EFFECT --- */
+$(document).ready(function() {
+    
+    // 1. Click Star Card (Go to Poem)
     $('.star-card').on('click', function() {
         var target = $(this).attr('data-target');
         
         $('#poem-list').fadeOut(300, function() {
             $(this).addClass('hidden');
+            // The title will now appear instantly with the poem
             $('#' + target).fadeIn(400).removeClass('hidden');
-            
-            // Trigger typing on the title of the new poem
-            typeTitle($('#' + target).find('.section-title'));
         });
     });
 
-    // 3. Back Button Logic
+    // 2. Back Button Logic
     $('.back.button-nav').on('click', function() {
         var $currentPoem = $(this).closest('.poem');
         
@@ -576,8 +560,20 @@ $(document).ready(function() {
             $('#poem-list').fadeIn(400).removeClass('hidden');
         });
     });
-});	
+
+    // 3. Next Poem Logic
+    $('.next-poem').on('click', function() {
+        var target = $(this).attr('data-target');
+        var $currentPoem = $(this).closest('.poem');
+
+        $currentPoem.fadeOut(300, function() {
+            $currentPoem.addClass('hidden');
+            $('#' + target).fadeIn(400).removeClass('hidden');
+        });
+    });
+}); 
 })(jQuery);  
+
 
 
 
